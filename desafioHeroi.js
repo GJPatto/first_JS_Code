ef11d7e8aab44d666c1ebbbd5906d72400591cd9
GJPatto <134503562+GJPatto@users.noreply.github.com>
2023-12-05 08:14:42 -0300

    Add files via upload

---
+++ desafioHeroi.js
@@ -0,0 +1,28 @@
+let nomeHeroi = "Felipão DIO"
+
+let nivelXP = 10001
+
+if (nivelXP <=1000){
+    console.log("o Herói <de nome " + nomeHeroi + " está no Nível FERRO")
+}
+else if (nivelXP >=1001 && nivelXP <=2000 ){
+    console.log("o Herói de nome " + nomeHeroi + " está no Nível BRONZE")
+}
+else if (nivelXP >=2001 && nivelXP <=5000 ){
+    console.log("o Herói de nome " + nomeHeroi + " está no Nível PRATA")
+}
+else if (nivelXP >=5001 && nivelXP <=7000 ){
+    console.log("o Herói de nome " + nomeHeroi + " está no Nível OURO")
+}
+else if (nivelXP >=7001 && nivelXP <=8000 ){
+    console.log("o Herói de nome " + nomeHeroi + " está no Nível PLATINA")
+}
+else if (nivelXP >=8001 && nivelXP <=9000 ){
+    console.log("o Herói de nome " + nomeHeroi + " está no Nível ASCENDENTE")
+}
+else if (nivelXP >=9001 && nivelXP <=10000 ){
+    console.log("o Herói de nome " + nomeHeroi + " está no Nível IMORTAL")
+}
+else if (nivelXP > 10000 ){
+    console.log("o Herói de nome " + nomeHeroi + " está no Nível RADIANTE")
+}
